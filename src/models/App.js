009f30bb704d26de5d45aafd22c7fa06af5e19ea
LordBigglesworth <javanpohl@gmail.com>
2021-06-16 17:56:08 -0600

    pulled the home page data out of App and put it into a HomePage.js file in views

---
+++ src/models/App.js
@@ -0,0 +1,7 @@
+import HomePage from '../views/HomePage'
+
+function App() {
+ return	<HomePage />
+}
+
+export default App;
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
+import './HomePage.css'
 
-function App() {
-  return (
+function HomePage() {
+	return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default HomePage
